9427e0e079b6b4070ed4908a726dfb67d0a10c57
PaulKolesnik <73100166+PaulKolesnik@users.noreply.github.com>
2021-06-01 22:13:56 +0300

    Add files via upload

---
+++ histograma.py
@@ -0,0 +1,17 @@
+import cv2
+import numpy as np
+from matplotlib import pyplot as plt
+
+# image path
+
+def runHistograma(path):
+
+    # using imread()
+    img = cv2.imread(path, cv2.IMREAD_GRAYSCALE)
+    cv2.imshow('Cat', img)
+
+    dst = cv2.calcHist(img, [0], None, [256], [0, 256])
+
+    plt.hist(img.ravel(), 256, [0, 256])
+    plt.title('Histogram for gray scale image')
+    plt.show()
